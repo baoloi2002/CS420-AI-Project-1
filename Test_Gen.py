140a4643f2aa2c39f7a3aab86150293a11646197
baoloi2002 <59519701+baoloi2002@users.noreply.github.com>
2022-11-14 22:05:06 +0700

    Test gen

---
+++ Test_Gen.py
@@ -0,0 +1,26 @@
+import random
+
+
+def main(path, size):
+
+    capacity = random.randint(5*size, 15*size)
+    numClasses = random.randint(5, 10)
+
+    weights = [random.randint(1,100) for i in range(size)]
+    values = [random.randint(1,200) for i in range(size)]
+    classes = [random.randint(1,numClasses) for i in range(size)]
+    
+    f = open(path, 'w')
+    f.write(str(capacity) + "\n")
+    f.write(str(numClasses) + "\n")
+    f.write(", ".join([str(u) for u in weights]) + "\n")
+    f.write(", ".join([str(u) for u in values]) + "\n")
+    f.write(", ".join([str(u) for u in classes]) + "\n")
+    f.close()
+
+
+
+if __name__ == '__main__':
+    size = [10, 20, 30, 40, 40, 100, 200, 500, 800, 1000]    
+    for i in range(len(size)):
+        main("INPUT/INPUT_" + str(i) + ".txt", size[i])
