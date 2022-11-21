91ebe0eb3c8dfe6f5d57891e75dd3a44cf782d0f
baoloi2002 <59519701+baoloi2002@users.noreply.github.com>
2022-11-21 21:44:35 +0700

    update B&B vip

--- knapsack/branching.py
+++ knapsack/branching.py
@@ -13,6 +13,19 @@ sys.setrecursionlimit(2000)
 
 debug = False
 
+def isOk(cur, w):
+    d = []
+    for i in range(cur, size):
+        d.append([values[ind[i]]/weights[ind[i]], weights[ind[i]]])
+    d.sort(reverse=True, key = lambda x: x[0])
+    res = 0
+    i = 0
+    while w>0 and i < len(d):
+        res += d[i][0]*d[i][1]
+        w -= d[i][1]
+        i += 1
+    return res
+
 def Try(cur, cap = 0, val = 0, cntClass = 0):
     global f, best, bestWay
     # Current cap > Capacity
@@ -23,12 +36,19 @@ def Try(cur, cap = 0, val = 0, cntClass = 0):
     if cntClass == (1<<numClasses)-1 and val > best:
         best = val
         bestWay = list(f)
+        print(best)
     
     if cur == size:
         return
 
+    if isOk(cur, capacity-cap) + val <= best:
+        return
+
     # Future value + Current value < Optimal value
     if val + suffixSumVal[cur] <= best:
+        return
+    # Future value + Current value < Optimal value
+    if val + suffixRate[cur]*(capacity-cap) <= best:
         return
 
     # Future number of class + Current number of class < Number of class
@@ -37,29 +57,34 @@ def Try(cur, cap = 0, val = 0, cntClass = 0):
 
     for i in range(1,-1,-1):
         f[cur] = i
-        Try(cur+1, cap + i*weights[cur], val + i*values[cur], cntClass|(i*(1<<(classes[cur]))))
+        Try(cur+1, cap + i*weights[ind[cur]], val + i*values[ind[cur]], cntClass|(i*(1<<(classes[ind[cur]]))))
         f[cur] = 0
 
 
 def pre_calculate():
-    global suffixSumVal, suffixNumClass, size, classes, values
+    global suffixSumVal, suffixNumClass, size, classes, values, suffixRate
     suffixSumVal = [0 for u in range(size)]
     suffixNumClass = [0 for u in range(size)]
+    suffixRate = [0 for u in range(size)]
     
     for i in range(size-1, -1, -1):
         if i+1 < size:
             suffixNumClass[i] = suffixNumClass[i+1]
             suffixSumVal[i] = suffixSumVal[i+1]
-        suffixSumVal[i] += values[i]
-        suffixNumClass[i] |= 1<<(classes[i])
+            suffixRate[i] = suffixRate[i+1]
+        suffixRate[i] = max(suffixRate[i], values[ind[i]]/weights[ind[i]])
+        suffixSumVal[i] += values[ind[i]]
+        suffixNumClass[i] |= 1<<(classes[ind[i]])
 
 def solve(_size, _capacity, _numClasses, _weights, _values, _classes):
-    global size, capacity, numClasses, weights, values, classes
+    global size, capacity, numClasses, weights, values, classes, ind
     size, capacity, numClasses, weights, values, classes = _size, _capacity, _numClasses, _weights, _values, _classes
 
     global best, bestWay, f
     best, bestWay = -1, []
     f = [0 for _ in range(size)]
+    ind = [u for u in range(size)]
+    ind.sort(reverse=True, key=lambda x: values[x]/weights[x])
 
     # pre-calculate 
     pre_calculate()
