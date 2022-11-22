3476f4a91a9be8745eed20e8f2244978895471c2
baoloi2002 <59519701+baoloi2002@users.noreply.github.com>
2022-11-22 22:45:56 +0700

    update brand

--- Test_Gen.py
+++ Test_Gen.py
@@ -27,13 +27,40 @@ def main(path, size):
     f.write(", ".join([str(u) for u in classes]) + "\n")
     f.close()
 
+def bigtest(path, size):
+
+    capacity = random.randint(5*size, 6*size)
+    numClasses = 10
+    weights = [random.randint(5,10) for i in range(size)]
+    values = [random.randint(1,10) for i in range(size)]
+    classes = [random.randint(1,numClasses) for i in range(size)]
+
+    for i in range(numClasses):
+        classes[i] = i+1
+    random.shuffle(classes)
+    
+    
+    f = open(path, 'w')
+    f.write(str(capacity) + "\n")
+    f.write(str(numClasses) + "\n")
+    f.write(", ".join([str(u) for u in weights]) + "\n")
+    f.write(", ".join([str(u) for u in values]) + "\n")
+    f.write(", ".join([str(u) for u in classes]) + "\n")
+    f.close()
 
 
 if __name__ == '__main__':
     index = 0
+    for i in range(5):
+        bigtest("BigTest/INPUT_" + str(index) + ".txt", 1000)
+        index += 1
+
+"""
     for i in range(10):
         main("INPUT/INPUT_" + str(index) + ".txt", i*4 + 4)
         index += 1
     for i in range(10):
         main("INPUT/INPUT_" + str(index) + ".txt", i*100 + 100)
         index += 1
+"""
+    
